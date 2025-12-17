f183d3ec3c9652b6d89773c158f061aac778abd9
Aadyn <ajgoodlet@gmail.com>
2025-12-17 21:55:29 +1000

    Add points

---
+++ point.lua
@@ -0,0 +1,21 @@
+---@class Point
+---@field x number
+---@field y number
+
+local Point = {}
+Point.__index = Point
+
+---@param x number
+---@param y number
+---@return Point
+function Point.new(x, y)
+  local self = {}
+
+  self.x = x or 0
+  self.y = y or 0
+
+  setmetatable(self, Point)
+  return self
+end
+
+return Point
